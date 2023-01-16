b4df8698fd7f60b395acfc287eb54a35b1923e53
Erik <eriksn07@gmail.com>
2023-01-16 18:27:20 -0300

    Feat: Page SignUp

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import { Login } from "../Pages/Login";
+import { SignUp } from "../Pages/SignUp";
 
 
 export const RoutesMain = () => { 
@@ -7,6 +8,7 @@ export const RoutesMain = () => {
 return(
     <Routes> 
        <Route path="/login" element={<Login/>}/>
+       <Route path="/signUp" element={<SignUp/>}/>
     </Routes>
 )
 
